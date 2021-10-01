94e402901de5e1deeb2e9b72bd24c9a8cdec46eb
Estevao <estevaom404@gmail.com>
2021-10-01 14:26:05 -0300

    Primeiro commit

---
+++ prototipo/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/imoveis', 'PropertyController@index');
+
+Route::get('/imoveis/novo', 'PropertyController@create');
+Route::post('/imoveis/store', 'PropertyController@store');
+
+Route::get('/imoveis/{name}', 'PropertyController@show');
+
+Route::get('/imoveis/editar/{name}', 'PropertyController@edit');
+Route::put('/imoveis/update/{name}', 'PropertyController@update');
+
+Route::get('/imoveis/remover/{name}', 'PropertyController@destroy');
